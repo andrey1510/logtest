c495203495bf5ea5a4ecdc7355c80d067a91bf9f
andrey1510 <andrey.ivn15@gmail.com>
2025-12-25 23:52:03 +0300

    updated

--- src/main/java/com/logtest/masker/processors/ValueProcessor.java
+++ src/main/java/com/logtest/masker/processors/ValueProcessor.java
@@ -5,38 +5,63 @@ import com.logtest.masker.patterns.MaskPatterns;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.temporal.Temporal;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.Map.entry;
 
 public class ValueProcessor {
 
+    private static final Map<MaskPatternType, Function<String, String>> STRING_MASKERS;
+    private static final Map<MaskPatternType, Function<Temporal, Temporal>> TEMPORAL_MASKERS;
+
+    static {
+        STRING_MASKERS = new EnumMap<>(Map.ofEntries(
+            entry(MaskPatternType.BALANCE, MaskPatterns::maskBalance),
+            entry(MaskPatternType.CONFIDENTIAL_NUMBER, MaskPatterns::maskConfidentialNumber),
+            entry(MaskPatternType.EMAIL, MaskPatterns::maskEmail),
+            entry(MaskPatternType.FULL_ADDRESS, MaskPatterns::maskFullAddress),
+            entry(MaskPatternType.INN, MaskPatterns::maskInn),
+            entry(MaskPatternType.ISSUER_NAME, MaskPatterns::maskIssuerName),
+            entry(MaskPatternType.ISSUER_CODE, MaskPatterns::maskIssuerCode),
+            entry(MaskPatternType.JWT_TYK_API_KEY_IP_ADDRESS, MaskPatterns::maskJwtTykApiKeyIpAddress),
+            entry(MaskPatternType.KPP, MaskPatterns::maskKpp),
+            entry(MaskPatternType.NAME, MaskPatterns::maskName),
+            entry(MaskPatternType.OGRNUL_OR_OGRNIP, MaskPatterns::maskOgrnUlOrOgrnIp),
+            entry(MaskPatternType.OKPO, MaskPatterns::maskOkpo),
+            entry(MaskPatternType.OTHER_DUL_NUMBER, MaskPatterns::maskOtherDulNumber),
+            entry(MaskPatternType.OTHER_DUL_SERIES, MaskPatterns::maskOtherDulSeries),
+            entry(MaskPatternType.SNILS, MaskPatterns::maskSnils),
+            entry(MaskPatternType.PASSPORT, MaskPatterns::maskPassport),
+            entry(MaskPatternType.PASSPORT_NUMBER, MaskPatterns::maskPassportNumber),
+            entry(MaskPatternType.PASSPORT_SERIES, MaskPatterns::maskPassportSeries),
+            entry(MaskPatternType.PAN, MaskPatterns::maskPan),
+            entry(MaskPatternType.PIN, MaskPatterns::maskPin),
+            entry(MaskPatternType.PHONE, MaskPatterns::maskPhoneNumber),
+            entry(MaskPatternType.TEXT_FIELD, MaskPatterns::maskTextField)
+        ));
+
+        TEMPORAL_MASKERS = new EnumMap<>(Map.ofEntries(
+            entry(MaskPatternType.LOCAL_DATE,
+                temporal -> temporal instanceof LocalDate date
+                    ? MaskPatterns.maskLocalDate(date)
+                    : temporal),
+
+            entry(MaskPatternType.OFFSET_DATE_TIME,
+                temporal -> temporal instanceof OffsetDateTime dateTime
+                    ? MaskPatterns.maskOffsetDateTime(dateTime)
+                    : temporal)
+        ));
+    }
+
     public static Object processValue(MaskPatternType type, Object value) {
-        return switch (type) {
-            case LOCAL_DATE -> value instanceof LocalDate date ?
-                MaskPatterns.maskLocalDate(date) : (Temporal) value;
-            case OFFSET_DATE_TIME -> value instanceof OffsetDateTime dateTime ?
-                MaskPatterns.maskOffsetDateTime(dateTime) : (Temporal) value;
-            case BALANCE -> MaskPatterns.maskBalance((String) value);
-            case CONFIDENTIAL_NUMBER -> MaskPatterns.maskConfidentialNumber((String) value);
-            case EMAIL -> MaskPatterns.maskEmail((String) value);
-            case FULL_ADDRESS -> MaskPatterns.maskFullAddress((String) value);
-            case INN -> MaskPatterns.maskInn((String) value);
-            case ISSUER_NAME -> MaskPatterns.maskIssuerName((String) value);
-            case ISSUER_CODE -> MaskPatterns.maskIssuerCode((String) value);
-            case JWT_TYK_API_KEY_IP_ADDRESS -> MaskPatterns.maskJwtTykApiKeyIpAddress((String) value);
-            case KPP -> MaskPatterns.maskKpp((String) value);
-            case NAME -> MaskPatterns.maskName((String) value);
-            case OGRNUL_OR_OGRNIP -> MaskPatterns.maskOgrnUlOrOgrnIp((String) value);
-            case OKPO -> MaskPatterns.maskOkpo((String) value);
-            case OTHER_DUL_NUMBER -> MaskPatterns.maskOtherDulNumber((String) value);
-            case OTHER_DUL_SERIES -> MaskPatterns.maskOtherDulSeries((String) value);
-            case SNILS -> MaskPatterns.maskSnils((String) value);
-            case PASSPORT -> MaskPatterns.maskPassport((String) value);
-            case PASSPORT_NUMBER -> MaskPatterns.maskPassportNumber((String) value);
-            case PASSPORT_SERIES -> MaskPatterns.maskPassportSeries((String) value);
-            case PAN -> MaskPatterns.maskPan((String) value);
-            case PIN -> MaskPatterns.maskPin((String) value);
-            case PHONE -> MaskPatterns.maskPhoneNumber((String) value);
-            case TEXT_FIELD -> MaskPatterns.maskTextField((String) value);
-            default -> value;
-        };
+        if (value instanceof String stringValue && STRING_MASKERS.containsKey(type)) {
+            return STRING_MASKERS.get(type).apply(stringValue);
+        } else if (value instanceof Temporal temporalValue && TEMPORAL_MASKERS.containsKey(type)) {
+            return TEMPORAL_MASKERS.get(type).apply(temporalValue);
+        } else {
+            return value;
+        }
     }
 }
